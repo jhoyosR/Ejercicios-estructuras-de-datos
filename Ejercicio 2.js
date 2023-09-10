1e1b1e3265b2e2ba88016af04f7c004ad655ef10
jhoyosR <jhoyos0410@cue.edu.co>
2023-09-10 16:11:02 -0500

    Hotel

--- Ejercicio 2.js
+++ Ejercicio 2.js
@@ -12,12 +12,12 @@ function Reserva(nombre, paisOrigen, tipoHabitacion, fumador, numeroPersonas, pe
   // Crear el hotel y sus habitaciones disponibles
   const hotel = {
     habitaciones: {
-      individual: { capacidadMaxima: 2, reservadas: 0 },
-      doble: { capacidadMaxima: 4, reservadas: 0 },
-      familiar: { capacidadMaxima: 6, reservadas: 0 },
+      individual: { capacidadMaxima: 2, reservadas: 0, fumadores: 0 },
+      doble: { capacidadMaxima: 4, reservadas: 0, fumadores: 0 },
+      familiar: { capacidadMaxima: 6, reservadas: 0, fumadores: 0 },
     },
     reservas: [],
-    mascotasAceptadas: ["familiar"], // Solo habitaciones familiares permiten mascotas
+    mascotasOpcionales: ["familiar"], // Habitaciones familiares permiten mascotas opcionalmente
   }
   
   // Función para realizar una reserva
@@ -34,11 +34,11 @@ function Reserva(nombre, paisOrigen, tipoHabitacion, fumador, numeroPersonas, pe
       return;
     }
   
-    if (tipoHabitacion === "familiar" && !trajoMascota) {
-      console.log("Las habitaciones familiares requieren mascota");
-      return;
-    }
-  
+    if (tipoHabitacion !== "familiar" && trajoMascota) {
+        console.log("No se permiten mascotas en habitaciones individuales o dobles");
+        return;
+      }
+
     if (hotel.reservas.length >= 10) {
       console.log("El hotel está lleno, no se pueden hacer más reservas");
       return;
@@ -48,21 +48,28 @@ function Reserva(nombre, paisOrigen, tipoHabitacion, fumador, numeroPersonas, pe
     hotel.reservas.push(reserva);
     habitacion.reservadas += 1;
   
+    if (fumador) {
+      habitacion.fumadores += 1;
+    }
+  
     console.log("Reserva realizada con éxito.");
   }
   
   // Ejemplo de reservas
-  hacerReserva("Juan", "España", "individual", false, 1, "2023-09-15", false);
-  hacerReserva("Ana", "Francia", "familiar", true, 4, "2023-09-20", true);
-  hacerReserva("Pedro", "México", "doble", false, 3, "2023-09-25", false);
+  hacerReserva("Juan", "España", "individual", true, 1, "2023-09-15", false);
+  hacerReserva("Ana", "Francia", "familiar", false, 4, "2023-09-20", true);
+  hacerReserva("Pedro", "México", "doble", true, 3, "2023-09-25", false);
   
-  // Mostrar el número de habitaciones reservadas en cada tipo
+  // Mostrar el número de habitaciones reservadas y fumadores en cada tipo
   console.log("Habitaciones reservadas:");
   for (const tipo in hotel.habitaciones) {
     console.log(`${tipo}: ${hotel.habitaciones[tipo].reservadas}`);
+    console.log(`Fumadores en ${tipo}: ${hotel.habitaciones[tipo].fumadores}`);
   }
   
   // Mostrar todas las reservas
   console.log("Todas las reservas:");
   console.log(hotel.reservas);
   
+// Mostrar la cantidad de reservas
+console.log(`Total de reservas realizadas: ${hotel.reservas.length}`);
